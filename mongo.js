e0e1e4f5b57412bbae2d3045d9192b1125e277a3
Sini Saarinen <sini.saarinen@hotmail.fi>
2023-02-12 14:45:30 +0200

    try to fix MongoDB connection problems on Render

--- mongo.js
+++ mongo.js
@@ -7,8 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url =
-    `mongodb+srv://fullstack:${password}@cluster0.oegu6.mongodb.net/personApp?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
